10760159fa74362cb6d21fadb4a371eae360a3e6
Antonio <antonio.b.fantoni@gmail.com>
2025-05-01 20:05:41 -0300

    criação do dir python_udemy e arquivo dados_elementares

---
+++ 2_primeiros_passos_python_udemy2025/dados_elementares.py
@@ -0,0 +1,11 @@
+#Python flix
+name = "Top Gun Maverick"
+yearLaunch = 2023
+noteMovie = 9.5
+planIncluded = False
+
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
+print(type(planIncluded))
+
